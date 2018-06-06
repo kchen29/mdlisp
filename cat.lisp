0b0f8a3d8efe608e43ee712dd2a4b2dc6626d292
kchen29 <kchen29@stuy.edu>
2018-06-06 17:11:32 -0400

    ellipsoid for cat

--- cat.lisp
+++ cat.lisp
@@ -5,6 +5,10 @@
 (defmacro part (name &body body)
   (declare (ignore name))
   `(temp ,@body))
+(defun ellipsoid (x y z r sx sy sz)
+  (temp
+   (scale sx sy sz)
+   (sphere x y z r)))
 
 (animate "cat" 60
     ((body (vary (0 14 0 1)
@@ -35,118 +39,91 @@
     (rotate y 5)
     (rotate x 5)
     (rotate z 17 body)
-    (temp
-      (scale 2 1 1)
-      (sphere 0 0 0 62))
+    (ellipsoid 0 0 0 62 2 1 1)
     (part body2
       (move -95 15 0)
       (temp
-        (rotate z -10)
-        (scale 1.16 .85 1)
-        (sphere 0 0 0 62))
+       (rotate z -10)
+       (ellipsoid 0 0 0 62 1.16 .85 1))
       (part left-arm1
         (move -20 30 55)
         (rotate z 30 flr)
         (rotate z -10)
-        (temp 
-          (scale 1 2.4 1)
-          (sphere 0 -27 0 27))
+        (ellipsoid 0 -27 0 27 1 2.4 1)
         (part left-arm2
           (move 0 -120 0)
           (rotate z 108 fe)
           (rotate z -18)
-          (scale 3.5 1 1)
-          (sphere -14 0 0 14)))
+          (ellipsoid -14 0 0 14 3.5 1 1)))
       (part right-arm1
         (move -20 30 -55)
         (rotate z 30 frr)
         (rotate z -10)
-        (temp
-          (scale 1 2.4 1)
-          (sphere 0 -27 0 27))
+        (ellipsoid 0 -27 0 27 1 2.4 1)
         (part right-arm2
           (move 0 -120 0)
           (rotate z 108 fe)
           (rotate z -18)
-          (scale 3.5 1 1)
-          (sphere -14 0 0 14)))
+          (ellipsoid -14 0 0 14 3.5 1 1)))
       (part head
         (move -65 15 0)
         (rotate z -17 body)
-        (temp
-          (scale 1 1 1.1)
-          (sphere 0 0 0 50))
+        (ellipsoid 0 0 0 50 1 1 1.1)
         (part muzzle
           (move -38 -25 0)
           (rotate z -25)
-          (scale 2.5 1 3.5)
-          (sphere 0 0 0 10))
+          (ellipsoid 0 0 0 10 2.5 1 3.5))
         (part left-ear
           (move 5 34 45)
           (rotate x 20)
           (rotate z -10)
-          (scale 1 2.1 1)
-          (sphere 0 0 0 11))
+          (ellipsoid 0 0 0 11 1 2.1 1))
         (part right-ear
           (move 0 34 -45)
           (rotate x -20)
           (rotate z -10)
-          (scale 1 2.1 1)
-          (sphere 0 0 0 11))))
+          (ellipsoid 0 0 0 11 1 2.1 1))))
     (part tail1
       (move 118 40 0)
       (temp
         (rotate z -5)
-        (scale 4.6 1 1)
-        (sphere 0 0 0 14))
+        (ellipsoid 0 0 0 14 4.6 1 1))
       (part tail2
         (move 65 -3 0)
         (temp
           (rotate z 4)
-          (scale 3.37 1 1)
-          (sphere 0 0 0 11))
+          (ellipsoid 0 0 0 11 3.37 1 1))
         (part tail3
           (move 46 0 0)
           (rotate z -10)
-          (scale 3.4 1 1)
-          (sphere 0 0 0 8))))
+          (ellipsoid 0 0 0 8 3.4 1 1))))
     (part left-leg1
       (move 100 30 50)
       (rotate z -75 rlr)
       (rotate z 35)
-      (temp
-        (scale 1 1.4 1)
-        (sphere 0 -40 0 40))
+      (ellipsoid 0 -40 0 40 1 1.4 1)
       (part left-leg2
         (move -10 -95 0)
         (rotate z 50 rc)
         (rotate z -20)
-        (temp
-          (scale 2 1 1)
-          (sphere 30 0 0 30))
+        (ellipsoid 30 0 0 30 2 1 1)
         (part left-foot
           (move 120 0 0)
           (rotate z -110 rc)
           (rotate z 70)
-          (scale 1 4 1)
-          (sphere 0 -14 0 14))))
+          (ellipsoid 0 -14 0 14 1 4 1))))
     (part right-leg1
       (move 100 30 -50)
       (rotate z -75 rrr)
       (rotate z 35)
-      (temp
-        (scale 1 1.4 1)
-        (sphere 0 -40 0 40))
+      (ellipsoid 0 -40 0 40 1 1.4 1)
       (part right-leg2
         (move -10 -95 0)
         (rotate z 50 rc)
         (rotate z -20)
-        (temp
-          (scale 2 1 1)
-          (sphere 30 0 0 30))
+        (ellipsoid 30 0 0 30 2 1 1)
         (part right-foot
           (move 120 0 0)
           (rotate z -110 rc)
           (rotate z 70)
-          (scale 1 4 1)
-          (sphere 0 -14 0 14))))))
+          (ellipsoid 0 -14 0 14 1 4 1))))))
