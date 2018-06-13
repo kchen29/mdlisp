99f6887d418deeb8d1718ab81cac52b594dfb3c4
kchen29 <kchen29@stuy.edu>
2018-06-13 00:09:34 -0400

    parameters

--- mdlisp-examples/anaglyph-test.lisp
+++ mdlisp-examples/anaglyph-test.lisp
@@ -1,9 +1,9 @@
 (load "mdlisp-examples/anaglyph.lisp")
 
-(with-anaglyph 0 2
+(with-anaglyph 2 2.6
   (box 0 300 0 100 100 100)
-  (sphere 250 250 250 100)
-  (move 450 250 150)
+  (sphere 250 250 -250 100)
+  (move 450 250 -150)
   (rotate z 90)
   (torus 0 0 0 20 100))
 (display)
